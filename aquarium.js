17a9c133d552988fb89348affaaa4e7f9ddea590
ryan <Ryanor27@bergen.org>
2024-10-17 09:32:15 -0400

    done

--- aquarium.js
+++ aquarium.js
@@ -4,6 +4,8 @@ let aquariumName = "My Aquarium";
 let foodX, foodY;
 let foodDropped = false;
 let bubbles = [];
+let fish2X, fish2Y, fishSize2;
+let fish2Color
 
 
 function setup() {
@@ -12,6 +14,10 @@ function setup() {
   fishY = height / 2;
   fishSize = 50;
   fishColor = color(200, 100, 100);
+  fish2X = width / 4
+  fish2Y = height / 2
+  fish2Size = 50;
+  fish2Color = color("blue");
 
 
   // Initialize bubbles
@@ -38,10 +44,27 @@ function draw() {
   ellipse(fishX, fishY, fishSize, fishSize / 2);
   triangle(fishX - fishSize / 2, fishY, fishX - fishSize, fishY - fishSize / 4, fishX - fishSize, fishY + fishSize / 4);
 
+  // Draw fish2
+  fill(fish2Color);
+  ellipse(fish2X, fish2Y, fish2Size, fish2Size / 2);
+  triangle(fish2X - fish2Size / 2, fish2Y, fish2X - fish2Size, fish2Y - fish2Size / 4, fish2X - fish2Size, fish2Y + fish2Size / 4);
+
 
   // Fish follows mouse
   fishX = lerp(fishX, mouseX, 0.05);
   fishY = lerp(fishY, mouseY, 0.05);
+
+  // Fish to uses arrows
+  if (keyCode === UP_ARROW) {
+    fish2Y -= 10;
+  } else if (keyCode === DOWN_ARROW) {
+    fish2Y += 10;
+  } else if (keyCode === LEFT_ARROW) {
+    fish2X -= 10;
+  } else if (keyCode === RIGHT_ARROW) {
+    fish2X += 10;
+  }
+}
 
 
   // Draw bubbles
@@ -75,7 +98,15 @@ function draw() {
   } else {
     fishColor = color(200, 100, 100);
   }
-}
+
+  
+  // The same as first fish near food but just dif coordinates
+  if (dist(fish2X, fish2Y, foodX, foodY) < fish2Size / 2 && foodDropped) {
+    fish2Color = color(100, 255, 150);
+    foodDropped = false;
+  } else {
+    fish2Color = color("blue");
+  }
 
 
 function keyPressed() {
